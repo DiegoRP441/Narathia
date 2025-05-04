3cd3c3a1221160896925c588d013ee7565b7e416
DiegoRP441 <157211411+DiegoRP441@users.noreply.github.com>
2025-05-04 22:41:28 +0200

    gitignore

--- backend/create_tables.sql
+++
@@ -1,9 +0,0 @@
--- Crear tabla de partidas
-CREATE TABLE IF NOT EXISTS partidas (
-    id SERIAL PRIMARY KEY,
-    usuario_id INTEGER NOT NULL REFERENCES usuarios(id) ON DELETE CASCADE,
-    nombre VARCHAR(255) NOT NULL,
-    estado JSONB NOT NULL,
-    ultima_modificacion TIMESTAMP NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
-); 
